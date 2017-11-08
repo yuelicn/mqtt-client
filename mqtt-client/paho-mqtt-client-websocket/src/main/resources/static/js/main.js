96db2fa8dcdb1fc19b67a9af986c74d83aaf9587
yue.li <yue.li3@21vianet.com>
2017-11-08 17:29:16 +0800

    update main.js add constructor
    Change-Id: If73b0a3c9e10c62f3b004711e5c382534981a7f1

--- mqtt-client/paho-mqtt-client-websocket/src/main/resources/static/js/main.js
+++ mqtt-client/paho-mqtt-client-websocket/src/main/resources/static/js/main.js
@@ -1,67 +1,77 @@
-var client;
-var reconnectTimeout = 5000;
+var websocket = function(config) {
 
-var config;
+	this.host = config.host;
+	this.port = config.port;
+	this.useTLS = config.useTLS;
+	this.topic = config.topic;
+	this.userName = config.userName;
+	this.password = config.password;
+	this.cleansession = config.cleansession;
+	this.reconnectTimeout = 5000;
+	var mqttClient;
 
-function MQTTconnect() {
-	client = new Paho.MQTT.Client(host, port, "clientId");
+	this.connect = function MQTTconnect() {
+		mqttClient = new Paho.MQTT.Client(this.host, this.port, "clientId");
 
-	var options = {
-		timeout : 10,
-		useSSL : useTLS,
-		cleanSession : cleansession,
-		onSuccess : onConnect,
-		onFailure : function(message) {
-			faildMssge(message.errorMessage, "Retrying", fialdMessageCallback)
-			setTimeout(MQTTconnect, reconnectTimeout);
+		var options = {
+			timeout : 10,
+			useSSL : this.useTLS,
+			cleanSession : this.cleansession,
+			onSuccess : onConnect,
+			onFailure : function(message) {
+				faildMssge(message.errorMessage, "Retrying",
+						fialdMessageCallback)
+				setTimeout(MQTTconnect, this.reconnectTimeout);
+			}
+		};
+
+		mqttClient.onConnectionLost = onConnectionLost;
+		mqttClient.onMessageArrived = onMessageArrived;
+
+		if (this.userName != null) {
+			options.userName = this.userName;
+			options.password = this.password;
 		}
-	};
 
-	client.onConnectionLost = onConnectionLost;
-	client.onMessageArrived = onMessageArrived;
-
-	if (userName != null) {
-		options.userName = userName;
-		options.password = password;
+		console.log("Host=" + host + ", port=" + port + " TLS = " + useTLS
+				+ " username=" + userName + " password=" + password);
+		mqttClient.connect(options);
+	}
+	function onConnect() {
+		mqttClient.subscribe(topic, {
+			qos : 0
+		});
 	}
 
-	console.log("Host=" + host + ", port=" + port + " TLS = " + useTLS
-			+ " username=" + userName + " password=" + password);
-	client.connect(options);
-}
-function onConnect() {
-	client.subscribe(topic, {
-		qos : 0
-	});
-}
+	function onConnectionLost(response) {
+		setTimeout(MQTTconnect, this.reconnectTimeout);
+		faildMssge("connection lost: " + responseObject.errorMessage,
+				". Reconnecting", fialdMessageCallback)
+	}
+	;
 
-function onConnectionLost(response) {
-	setTimeout(MQTTconnect, reconnectTimeout);
-	faildMssge("connection lost: " + responseObject.errorMessage, ". Reconnecting", fialdMessageCallback)
-};
+	function onMessageArrived(message) {
+		var topic = message.destinationName;
+		var payload = message.payloadString;
+		callback(topic, payload, messageCallback);
+	}
+	;
 
-function onMessageArrived(message) {
-	var topic = message.destinationName;
-	var payload = message.payloadString;
-	callback(topic, payload, messageCallback);
-};
+	function callback(topic, payload, messageCallback) {
+		messageCallback(topic, payload);
+	}
 
-function callback(topic, payload, messageCallback) {
-	messageCallback(topic, payload);
-}
+	function faildMssge(errorMessage, type, fialdMessageCallback) {
+		fialdMessageCallback(errorMessage, type);
+	}
 
-function faildMssge(errorMessage, type, fialdMessageCallback) {
-	fialdMessageCallback(errorMessage, type);
-}
-
-function sendMessage(topic, message){
-	var message = new Paho.MQTT.Message(message);
-	message.destinationName = topic;
-	message.qos=0;
-	client.send(message);
-	
+	this.sendMessage = function sendMessage(topic, message) {
+		var message = new Paho.MQTT.Message(message);
+		message.destinationName = topic;
+		message.qos = 0;
+		mqttClient.send(message);
+	}
 }
 
-$(document).ready(function() {
-	MQTTconnect();
-});
+// new MQTTconnect();
+
